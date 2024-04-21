615306e200b88a72a0586a2a55dc647ff931f644
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-04-20 23:56:27 -0500

    Headstone and Ozma test arenas using ArenaBoundsUnion

--- BossMod/Modules/Heavensward/Alliance/A24Ozma/A24Ozma.cs
+++ BossMod/Modules/Heavensward/Alliance/A24Ozma/A24Ozma.cs
@@ -1,7 +1,8 @@
 ﻿namespace BossMod.Heavensward.Alliance.A24Ozma;
 
 [ModuleInfo(BossModuleInfo.Maturity.WIP, Contributors = "CombatReborn Team", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 168, NameID = 4896)]
-public class A24Ozma(WorldState ws, Actor primary) : BossModule(ws, primary, new ArenaBoundsCircle(new(280, -410), 30))
+public class A24Ozma(WorldState ws, Actor primary) : BossModule(ws, primary, new ArenaBoundsUnion([new ArenaBoundsDonut(new(280, -410), 20, 30),
+new ArenaBoundsRect(new(260, -420), 5, 15, 240.Degrees()), new ArenaBoundsRect(new(280, -385), 5, 15, 180.Degrees()), new ArenaBoundsRect(new(300, -420), 5, 15, 110.Degrees())]))
 {
     protected override void DrawEnemies(int pcSlot, Actor pc)
     {
