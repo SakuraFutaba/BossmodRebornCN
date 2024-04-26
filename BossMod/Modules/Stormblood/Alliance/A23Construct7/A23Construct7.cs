88748a1486ac1d8a64799d416654fd5076d05527
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-04-26 01:02:14 -0500

    arena corrections, shapes and sizes

--- BossMod/Modules/Stormblood/Alliance/A23Construct7/A23Construct7.cs
+++ BossMod/Modules/Stormblood/Alliance/A23Construct7/A23Construct7.cs
@@ -3,5 +3,5 @@
 [ModuleInfo(BossModuleInfo.Maturity.WIP, Contributors = "CombatReborn Team", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 550, NameID = 7237)]
 public class A23Construct7 : BossModule
 {
-    public A23Construct7(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsCircle(new(800, -146), 30)) { }
+    public A23Construct7(WorldState ws, Actor primary) : base(ws, primary, new ArenaBoundsSquare(new(800, -141), 60)) { }
 }
